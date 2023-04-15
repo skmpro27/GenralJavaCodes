c356f8786f7e5a31a378970b1e3c0dcb7496c393
skmpro27 <kumar.skm03@gmail.com>
2023-04-16 03:22:02 +0530

    master
    One more solution added for streams

---
+++ src/fastestFifty/FastestFiftyUsingStreams.java
@@ -0,0 +1,42 @@
+package fastestFifty;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class FastestFiftyUsingStreams {
+
+    public static int sum;
+
+    public static void calculateFifty(List<Integer> runs) {
+        String result = IntStream.rangeClosed(9, runs.size())
+                .mapToObj(numberOfBalls -> {
+                    sum = IntStream.range(0, numberOfBalls)
+                            .map(runs::get)
+                            .sum();
+
+                    return IntStream.range(numberOfBalls, runs.size())
+                            .mapToObj(i -> {
+                                if (sum >= 50) {
+                                    return "Score is " + sum + " in " + numberOfBalls + "\nIt is scored between ball " +
+                                            (i - numberOfBalls) + " and " + i;
+                                } else {
+                                    sum = sum + runs.get(i) - runs.get(i - numberOfBalls);
+                                    return sum + " + " + runs.get(i) + " - " + runs.get(i - numberOfBalls) + " = " + sum;
+                                }
+                            })
+                            .filter(str -> str.startsWith("Score"))
+                            .findFirst();
+                })
+                .filter(Optional::isPresent)
+                .map(Optional::get).findFirst().orElse("not found");
+        System.out.println(result);
+    }
+
+
+    public static void main(String[] args) {
+        List<Integer> runs = Arrays.asList(6, 4, 1, 0, 6, 1, 2, 3, 6, 1, 6, 1, 6, 1, 6, 6, 1, 6, 6, 1, 0, 4);
+        calculateFifty(runs);
+    }
+}
